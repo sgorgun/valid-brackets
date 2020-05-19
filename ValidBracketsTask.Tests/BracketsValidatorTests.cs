05ce080016db72be2fe9a484c463e4ba6f1093ca
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-19 07:04:34 +0000

    add-tests-file

---
+++ ValidBracketsTask.Tests/BracketsValidatorTests.cs
@@ -0,0 +1,43 @@
+using System;
+using NUnit.Framework;
+
+namespace ValidBracketsTask.Tests
+{
+    [TestFixture]
+    public class BracketsValidatorTests
+    {
+        [TestCase("{{   {", ExpectedResult = true)]
+        [TestCase("[", ExpectedResult = true)]
+        [TestCase("{432   34}Test [Test][Test Test)", ExpectedResult = false)]
+        [TestCase("([([   {{ }}  ])])", ExpectedResult = true)]
+        [TestCase("{abc  ([b ]a)}", ExpectedResult = true)]
+        [TestCase("", ExpectedResult = true)]
+        [TestCase("Test Test Test", ExpectedResult = true)]
+        [TestCase("{abc   ([a{sas}b]a)}", ExpectedResult = true)]
+        [TestCase("ab  c([b] a)}", ExpectedResult = false)]
+        [TestCase("{abc(   [a{s  {as}b]a)}   }", ExpectedResult = false)]
+        [TestCase("()", ExpectedResult = true)]
+        [TestCase("(       )[  ]", ExpectedResult = true)]
+        [TestCase("()[]{}", ExpectedResult = true)]
+        [TestCase("[]  {}", ExpectedResult = true)]
+        [TestCase("{}", ExpectedResult = true)]
+        [TestCase("[]", ExpectedResult = true)]
+        [TestCase("[{()}]", ExpectedResult = true)]
+        [TestCase("(      {[ ]}    )", ExpectedResult = true)]
+        [TestCase("(", ExpectedResult = false)]
+        [TestCase(")", ExpectedResult = false)]
+        [TestCase("[", ExpectedResult = false)]
+        [TestCase("]", ExpectedResult = false)]
+        [TestCase("{", ExpectedResult = false)]
+        [TestCase("}", ExpectedResult = false)]
+        [TestCase("}Test", ExpectedResult = false)]
+        [TestCase("} ( )Test {", ExpectedResult = false)]
+        public bool CheckValidBracketsTests(string input)
+            => new BracketsValidator().IsValid(input);
+
+        [Test]
+        public void AreValidParentheses_StringIsNull_ThrowsArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => new BracketsValidator().IsValid(null),
+                "String cannot be null.");
+    }
+}
