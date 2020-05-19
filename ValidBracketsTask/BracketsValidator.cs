ad2f354a55c21c0596b67ad3b5d038239bf2d5b7
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-19 06:38:58 +0000

    add-task-file

---
+++ ValidBracketsTask/BracketsValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValidBracketsTask
+{
+    /// <summary>
+    /// Provides methods to validate of the correct placement of brackets in the string.
+    /// </summary>
+    public class BracketsValidator
+    {
+        //TODO: Add necessary code and remove this comment.
+        
+        /// <summary>
+        /// Validates of the correct placement of brackets in the string.
+        /// </summary>
+        /// <param name="input">String to validate.</param>
+        /// <returns>true if the pairs of brackets are placed correctly, otherwise - false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if string is null.</exception>
+        public bool IsValid(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
